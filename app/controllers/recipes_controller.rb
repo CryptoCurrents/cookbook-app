5375ca301d19723461f2653f5921a50b30dc995e
JackMarx <seriousfools@gmail.com>
2017-09-05 17:36:06 -0500

    seeds.rb

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,29 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
+  def new
+    
+  end
+
+  def create
+    recipe = Recipe.new(
+                        title: params[:title],
+                        chef: params[:chef],
+                        ingredients: params[:ingredients],
+                        directions: params[:directions]
+                        )
+    recipe.save
+  end
+
   def show
     @recipe = Recipe.find(params[:id])
   end
 end
+
+
+
+
+
+
+
+
